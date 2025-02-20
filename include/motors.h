8cd91bbd2dcd5b9c3530cb77bd17bebe212e6db2
wineT <tolya.bogomolov2019@gmail.com>
2025-02-21 00:10:27 +1000

    encoders support

--- include/motors.h
+++ include/motors.h
@@ -1,19 +1,23 @@
 #ifndef MOTORS_H
 #define MOTORS_H
 #include <Arduino.h>
+#include <Encoder.h>
 
 class Motor {
     public:
         Motor(uint8_t forward_pin, uint8_t backward_pin, uint8_t pwm_pin, uint8_t encoder_a_pin, uint8_t encoder_b_pin, int CPR);
+        ~Motor();
         void set(int speed);
-        int CPR;
+        const int CPR;
+
+        int target_velocity;
+        int current_velocity;
+
+        Encoder* encoder;
     private:
         uint8_t forward_pin;
         uint8_t backward_pin;
         uint8_t pwm_pin;
-        
-        uint8_t encoder_a_pin;
-        uint8_t encoder_b_pin;
 };
 
 #endif
--- src/motor.cpp
+++ src/motor.cpp
@@ -1,13 +1,15 @@
 #include "motors.h"
 
-Motor::Motor(uint8_t forward_pin, uint8_t backward_pin, uint8_t pwm_pin, uint8_t encoder_a_pin, uint8_t encoder_b_pin, int CPR) {
-    this->forward_pin = forward_pin;
-    this->backward_pin = backward_pin;
+Motor::Motor(uint8_t forward_pin, uint8_t backward_pin, uint8_t pwm_pin,
+             uint8_t encoder_a_pin, uint8_t encoder_b_pin, int CPR)
+    : forward_pin(forward_pin),
+      backward_pin(backward_pin),
+      pwm_pin(pwm_pin),
+      CPR(CPR) {
+    pinMode(forward_pin, OUTPUT);
+    pinMode(backward_pin, OUTPUT);
 
-    this->encoder_a_pin = encoder_a_pin;
-    this->encoder_b_pin = encoder_b_pin;
-
-    this->CPR = CPR;
+    encoder = new Encoder(encoder_a_pin, encoder_b_pin);
 }
 
 void Motor::set(int speed) {
@@ -15,3 +17,5 @@ void Motor::set(int speed) {
     digitalWrite(backward_pin, speed <= 0 ? HIGH : LOW);
     analogWrite(pwm_pin, speed);
 }
+
+Motor::~Motor() { delete encoder; }
