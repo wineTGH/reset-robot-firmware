baf23362a0d43fcc20e116ea82aa427a724ba03c
wineT <tolya.bogomolov2019@gmail.com>
2025-02-21 15:33:14 +1000

    Actuator class

---
+++ include/actuator.h
@@ -0,0 +1,17 @@
+#ifndef ACTUATOR_H
+#define ACTUATOR_H
+
+#include <Arduino.h>
+
+class Actuator {
+    public:
+        Actuator(uint8_t up_pin, uint8_t down_pin);
+        void up();
+        void down();
+
+    private:
+        uint8_t up_pin;
+        uint8_t down_pin;
+};
+
+#endif
---
+++ src/actuator.cpp
@@ -0,0 +1,16 @@
+#include "actuator.h"
+
+Actuator::Actuator(uint8_t up_pin, uint8_t down_pin): up_pin(up_pin), down_pin(down_pin) {
+    pinMode(up_pin, OUTPUT);
+    pinMode(down_pin, OUTPUT);
+}
+
+void Actuator::up() {
+    digitalWrite(up_pin, HIGH);
+    digitalWrite(down_pin, LOW);
+}
+
+void Actuator::down() {
+    digitalWrite(up_pin, LOW);
+    digitalWrite(down_pin, HIGH);
+}
